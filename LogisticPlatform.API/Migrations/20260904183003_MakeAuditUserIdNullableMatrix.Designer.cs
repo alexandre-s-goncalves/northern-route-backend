dacf2df517bb7a66c7dd4dc1f3cb2456bb7bf08e
Alexandre <alexandre.sgoncalves@outlook.com>
2026-09-04 15:30:20 -0300

    doc(database): generate incremental migration snapshot to enforce nullable audit schema structures

--- LogisticPlatform.API/Migrations/20260904183003_MakeAuditUserIdNullableMatrix.Designer.cs
+++ LogisticPlatform.API/Migrations/20260904183003_MakeAuditUserIdNullableMatrix.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LogisticPlatform.API.Common.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace LogisticPlatform.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260904183003_MakeAuditUserIdNullableMatrix")]
+    partial class MakeAuditUserIdNullableMatrix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
